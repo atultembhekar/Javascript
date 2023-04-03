18fda61eb4324f20282576cb62cc87591721e8e1
atultembhekar <127327717+atultembhekar@users.noreply.github.com>
2023-04-04 02:22:46 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,11 @@
+var website = "https://google.com/";
+var teacherName = 'Atul Tembhekar';
+
+console.log(website)
+
+if (teacherName=="Atul Tembhekar") {
+    let apples=5;
+    const height="6'0\"";
+    console.log("I have", apples, "apples");
+    console.log("I am", height);
+}
