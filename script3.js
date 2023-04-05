6efda4da76b2ae0e819be3a5a36173d773b1f341
atultembhekar <127327717+atultembhekar@users.noreply.github.com>
2023-04-06 00:54:14 +0530

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,17 @@
+//Javascript Data types
+const str = "A string"; //string
+const num = 3; //Number
+const flt = 3.14; //float
+const bool = true; // or "false"
+const list = ["Item1", 2, "Three", 3.14, []]  //Array=List
+const thing = null; //Has nothing in it
+const notDefined = undefined;  //currntly holds not value
+const getAge = function(){return 31;}   //This is a function
+const obj = {
+    'name': 'Atul',
+    'profession': 'coder'
+}
+
+//Object; can be accessed with obj['name']
+
+//I have used const so none of this can be reused whatsoever its set and forgotten
